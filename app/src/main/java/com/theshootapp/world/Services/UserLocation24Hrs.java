0887d70dc41c26b9b4c9c2c2b9b68c7cbcfbaab9
hamza5213 <35306029+hamza5213@users.noreply.github.com>
2018-06-25 00:48:16 +0500

    Merge branch 'master' of https://github.com/AgencyApp/Shoot
    # Conflicts:
    #	.idea/modules.xml
    #	app/build.gradle

--- app/src/main/java/com/theshootapp/world/Services/UserLocation24Hrs.java
+++ app/src/main/java/com/theshootapp/world/Services/UserLocation24Hrs.java
@@ -8,7 +8,10 @@ import android.location.Location;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.theshootapp.world.ModelClasses.LocationModel;
@@ -41,6 +44,7 @@ public class UserLocation24Hrs extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         locationReference= FirebaseDatabase.getInstance().getReference().child("UserLocation").child(FirebaseAuth.getInstance().getUid());
+        final GeoFire geoFire = new GeoFire(locationReference);
         long mLocTrackingInterval = 1000 * 60; // 5 sec
         float trackingDistance = 10f;
         LocationAccuracy trackingAccuracy = LocationAccuracy.HIGH;
@@ -53,16 +57,21 @@ public class UserLocation24Hrs extends Service {
         SmartLocation.with(getApplicationContext()).location().continuous().config(builder.build()).start(new OnLocationUpdatedListener() {
             @Override
             public void onLocationUpdated(Location location) {
-                LocationModel locationModel=new LocationModel(location.getLongitude(),location.getLatitude());
+                LocationModel locationModel = new LocationModel(location.getLongitude(), location.getLatitude());
                 sharedPreferences = getSharedPreferences("location", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("longitude",String.valueOf(location.getLongitude()) );
-                editor.putString("latitude",String.valueOf(location.getLatitude()) );
+                editor.putString("longitude", String.valueOf(location.getLongitude()));
+                editor.putString("latitude", String.valueOf(location.getLatitude()));
                 editor.commit();
-                locationReference.setValue(locationModel);
+                //  locationReference.setValue(locationModel);
+                geoFire.setLocation(FirebaseAuth.getInstance().getUid(), new GeoLocation(location.getLatitude(), location.getLongitude()), new GeoFire.CompletionListener() {
+                    @Override
+                    public void onComplete(String key, DatabaseError error) {
+
+                    }
+                });
             }
         });
-
         return START_STICKY;
     }
 }
